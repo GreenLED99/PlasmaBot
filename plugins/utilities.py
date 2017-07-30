913eae9bcd54c7c3627cf63ae6817177934a3390
ThePlasmaGuy <theplasmaguy2403@gmail.com>
2017-07-30 12:52:49 -0700

    Added <school and <work status commands, reworked afk command ignore

--- plugins/utilities.py
+++ plugins/utilities.py
@@ -22,8 +22,11 @@ class Status(Plugin):
         if not self.db.table('status').tableExists():
             self.db.table('status').init(STATUS_DEFAULTS)
 
-    @command('afk', 0, description='Set your state as AFK!', usage='afk [afk_message]')
+        self.status_ignore = ['restart', 'shutdown', 'afk', 'sleep', 'work', 'school']
+
+    @command('afk', 0, description='Set your status as "AFK" with an optional descriptive message', usage='afk [afk_message]')
     async def afk_command(self, author, content, user_mentions, role_mentions):
+        """Channel AFK command:  Allows users to set their status as 'AFK'"""
 
         afk_message = content.replace('\n', ' ').replace('@everyone', '@\u200beveryone').replace('@here', '@\u200bhere')
 
@@ -48,11 +51,11 @@ class Status(Plugin):
         else:
             self.db.table('status').insert(author.id, 'AFK', afk_message).into('USER_ID', 'STATUS', 'MESSAGE')
 
-        return ChannelResponse(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is AFK{}   {}'.format(author.display_name, ':' if len(afk_message) >= 1 else '', afk_message), icon_url=author.avatar_url), expire=None)
+        return ChannelResponse(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is AFK{}{}'.format(author.display_name, ':   ' if len(afk_message) >= 1 else '...', afk_message), icon_url=author.avatar_url), expire=None)
 
-    @command('sleep', 0, description='Set your state as asleep!', usage='sleep')
+    @command('sleep', 0, description='Set your status as "asleep"', usage='sleep')
     async def sleep_command(self, author):
-        """Channel AFK command: Allows users to set their state as asleep."""
+        """Channel sleep command: Allows users to set their status as 'asleep'"""
 
         states = self.db.table('status').select('STATUS').where('USER_ID').equals(author.id).execute()
         user_status = None
@@ -67,6 +70,42 @@ class Status(Plugin):
             self.db.table('status').insert(author.id, 'asleep', '').into('USER_ID', 'STATUS', 'MESSAGE')
 
         return ChannelResponse(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is now sleeping... 💤'.format(author.display_name), icon_url=author.avatar_url), expire=None)
+
+    @command('work', 0, description='Set your status as "at work"', usage='work')
+    async def work_command(self, author):
+        """Channel work command: Allows users to set their status as 'at work'"""
+
+        states = self.db.table('status').select('STATUS').where('USER_ID').equals(author.id).execute()
+        user_status = None
+
+        for user in states:
+            user_status = user[0]
+
+        if user_status:
+            self.db.table('status').update('STATUS').setTo('at work').where('USER_ID').equals(author.id).execute()
+            self.db.table('status').update('MESSAGE').setTo('').where('USER_ID').equals(author.id).execute()
+        else:
+            self.db.table('status').insert(author.id, 'at work', '').into('USER_ID', 'STATUS', 'MESSAGE')
+
+        return ChannelResponse(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is now at work... 💼'.format(author.display_name), icon_url=author.avatar_url), expire=None)
+
+    @command('school', 0, description='Set your status as "at school"', usage='school')
+    async def school_command(self, author):
+        """Channel school command: Allows users to set their status as 'at school'"""
+
+        states = self.db.table('status').select('STATUS').where('USER_ID').equals(author.id).execute()
+        user_status = None
+
+        for user in states:
+            user_status = user[0]
+
+        if user_status:
+            self.db.table('status').update('STATUS').setTo('at school').where('USER_ID').equals(author.id).execute()
+            self.db.table('status').update('MESSAGE').setTo('').where('USER_ID').equals(author.id).execute()
+        else:
+            self.db.table('status').insert(author.id, 'at school', '').into('USER_ID', 'STATUS', 'MESSAGE')
+
+        return ChannelResponse(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is now at school... 📚'.format(author.display_name), icon_url=author.avatar_url), expire=None)
 
     async def clear_status(self, user, channel):
         """Clear the status of a given user from the status database table"""
@@ -90,6 +129,8 @@ class Status(Plugin):
 
         afk_members = []
         sleep_members = []
+        work_members = []
+        school_members = []
 
         for user in mentions:
             states = self.db.table('status').select('STATUS', 'MESSAGE').where('USER_ID').equals(user.id).execute()
@@ -107,14 +148,24 @@ class Status(Plugin):
                 afk_members += [[user, message]]
             elif user_status == 'asleep':
                 sleep_members += [user]
+            elif user_status == 'at work':
+                work_members += [user]
+            elif user_status == 'at school':
+                school_members += [user]
 
         if len(offline_members) == 1:
             if len(afk_members) == 1:
                 async with channel.typing():
-                    await channel.send(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is AFK{}  {}'.format(afk_members[0][0].display_name, ':' if len(afk_members[0][1]) >= 1 else '', afk_members[0][1]), icon_url=afk_members[0][0].avatar_url), delete_after=15)
+                    await channel.send(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is AFK{}{}'.format(afk_members[0][0].display_name, ':   ' if len(afk_members[0][1]) >= 1 else '.', afk_members[0][1]), icon_url=afk_members[0][0].avatar_url), delete_after=15)
             elif len(sleep_members) == 1:
                 async with channel.typing():
                     await channel.send(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is sleeping.  💤'.format(sleep_members[0].display_name), icon_url=sleep_members[0].avatar_url), delete_after=15)
+            elif len(work_members) == 1:
+                async with channel.typing():
+                    await channel.send(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is at work.  💼'.format(work_members[0].display_name), icon_url=work_members[0].avatar_url), delete_after=15)
+            elif len(school_members) == 1:
+                async with channel.typing():
+                    await channel.send(embed=discord.Embed(color=discord.Colour.purple()).set_author(name='{} is at school.  📚'.format(school_members[0].display_name), icon_url=school_members[0].avatar_url), delete_after=15)
         elif len(offline_members) >= 1:
             async with channel.typing():
                 if len(offline_members) >= 10:
@@ -132,6 +183,7 @@ class Status(Plugin):
     @event
     async def on_message(self, message):
         """PLUGIN EVENT: Client.on_message()"""
-        if not message.content.lower().startswith('{}afk'.format(self.config['presence']['prefix'])) and not message.content.startswith('{}sleep'.format(self.config['presence']['prefix'])):
-            await self.clear_status(message.author, message.channel)
-            await self.announce_status(message.mentions, message.channel)
+        if not message.content == '':
+            if not message.content.lower().split()[0][len(self.config['presence']['prefix']):].strip() in self.status_ignore:
+                await self.clear_status(message.author, message.channel)
+                await self.announce_status(message.mentions, message.channel)
