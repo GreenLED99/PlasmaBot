3ee0f4fe3af900ede28cf1b073424ee8289c7e2a
ThePlasmaGuy <theplasmaguy2403@gmail.com>
2017-08-15 16:22:54 -0700

    Added mute and unmute commands

--- plugins/moderation.py
+++ plugins/moderation.py
@@ -24,6 +24,7 @@ class Moderation(Plugin):
             self.db.table('guilds').init(GUILD_DEFAULTS)
 
         self.permissions.register('manage_logs', False, 'Permissions')
+        self.permissions.register('text_mute_members', False, 'Moderation')
 
     def get_log_channel(self, guild):
         """Method that queries the database for the log channel in a given guild"""
@@ -82,3 +83,193 @@ class Moderation(Plugin):
             return ChannelResponse(content='Error Setting Moderation Log Channel for {}'.format(guild.name))
         else:
             return ChannelResponse(content='Moderation Log Channel set to {} for {}'.format(channel_mentions[0].mention, guild.name))
+
+    @command('mute', 0, description='Prevent a guild member from sending messages to a given text channel', usage='mute [user] (channel) [reason]', permission='administrator text_mute_members', private=False)
+    async def text_mute_command(self, author, channel, guild, content, user_mentions, channel_mentions, args):
+        """Command that prevents a guild member from sending messages to a given text channel"""
+        if not len(user_mentions) >= 1:
+            return ChannelResponse(send_help=True)
+
+        if not len(content) >= len(user_mentions[0].mention + ' '):
+            reason = 'No Reason Provided'
+        else:
+            reason = content[len(user_mentions[0].mention + ' '):]
+
+        user = user_mentions[0]
+
+        if user == author:
+            return ChannelResponse(content='*Muting one\'s self is rarely beneficial*')
+
+        if self.permissions.has_any_permission(['administrator', 'text_mute_members'], user, channel):
+            return ChannelResponse(content='*Member can not be muted due to elevated mute permissions*')
+
+        channel_perms = self.permissions.has_any_permission(['administrator', 'text_mute_members'], author, channel)
+        guild_perms = self.permissions.has_any_permission(['administrator', 'text_mute_members'], author, channel.guild)
+
+        target = None
+
+        if len(channel_mentions) >= 1:
+            if args[1] == channel_mentions[0].mention:
+                target = channel_mentions[0]
+                target_perms = self.permissions.has_any_permission(['administrator', 'text_mute_members'], author, target)
+
+                if not target.guild == guild:
+                    return ChannelResponse(content='*Passed Channel must belong to the current Guild*')
+
+                if not target_perms:
+                    return ChannelResponse(content='**INVALID Permissions**: {} does not have the `administrator` or `text_mute_members` permission in {}.'.format(author.display_name, target.mention))
+
+                if not len(reason) >= len(target.mention + ' '):
+                    reason = 'No Reason Provided'
+                else:
+                    reason = reason[len(target.mention + ' '):]
+
+        if (not target) and (not guild_perms):
+            target = channel
+
+        log_channel = self.get_log_channel(guild)
+
+        if target:
+            if not target.permissions_for(user).send_messages:
+                return ChannelResponse(content='*{} is already devoid of messaging permissions in {}*'.format(user, target.mention))
+
+            mute_message = await channel.send(content=self.filter_mentions('Muting {} in {}...'.format(user.display_name, target.mention)), delete_after=20)
+
+            try:
+                await target.set_permissions(user, reason='TEXT-MUTE: {}'.format(reason), send_messages=False)
+            except:
+                await mute_message.edit(content=self.filter_mentions('Error Muting {} in {}.'.format(user.display_name, target.mention)))
+                return
+
+            await mute_message.edit(content=self.filter_mentions('Muted {} in {}.'.format(user.display_name, target.mention)))
+
+            if log_channel:
+                log_embed = discord.Embed(color=discord.Colour.red()).set_author(name='MUTE: {} ({}#{})'.format(user.display_name, user.name, user.discriminator), icon_url=user.avatar_url)
+                log_embed.add_field(name='Reason', value=reason, inline=False).add_field(name='Channel', value=target.mention, inline=False).add_field(name='Responsible Moderator', value=author.display_name, inline=False)
+                await log_channel.send(embed=log_embed)
+
+            return
+        else:
+            mute_message = await channel.send(content=self.filter_mentions('Muting {}...'.format(user.display_name)), delete_after=20)
+
+            error_channels = []
+
+            if channel.permissions_for(user).send_messages:
+                try:
+                    await channel.set_permissions(user, reason='TEXT-MUTE: {}'.format(reason), send_messages=False)
+                except:
+                    error_channels += [channel]
+
+            for target in guild.text_channels:
+                if target.permissions_for(user).send_messages and not channel == target:
+                    try:
+                        await target.set_permissions(user, reason='TEXT-MUTE: {}'.format(reason), send_messages=False)
+                    except:
+                        error_channels += [target]
+
+            if error_channels:
+                await mute_message.edit(content=self.filter_mentions('Unable to mute {} in {} of {} channels'.format(user, len(error_channels), len(guild.text_channels))))
+            else:
+                await mute_message.edit(content=self.filter_mentions('Muted {}...'.format(user.display_name)))
+
+            if log_channel:
+                log_embed = discord.Embed(color=discord.Colour.red()).set_author(name='MUTE: {} ({}#{})'.format(user.display_name, user.name, user.discriminator), icon_url=user.avatar_url)
+                log_embed.add_field(name='Reason', value=reason, inline=False).add_field(name='Responsible Moderator', value=author.display_name, inline=False)
+                await log_channel.send(embed=log_embed)
+
+            return
+
+
+    @command('unmute', 0, description='Return messaging permissions to a given guild member', usage='mute [user] (channel) [reason]', permission='administrator text_mute_members', private=False)
+    async def text_unmute_command(self, author, channel, guild, content, user_mentions, channel_mentions, args):
+        """Return messaging permissions to a given guild member"""
+        if not len(user_mentions) >= 1:
+            return ChannelResponse(send_help=True)
+
+        if not len(content) >= len(user_mentions[0].mention + ' '):
+            reason = None
+        else:
+            reason = content[len(user_mentions[0].mention + ' '):]
+
+        user = user_mentions[0]
+
+        channel_perms = self.permissions.has_any_permission(['administrator', 'text_mute_members'], author, channel)
+        guild_perms = self.permissions.has_any_permission(['administrator', 'text_mute_members'], author, channel.guild)
+
+        target = None
+
+        if len(channel_mentions) >= 1:
+            if args[1] == channel_mentions[0].mention:
+                target = channel_mentions[0]
+                target_perms = self.permissions.has_any_permission(['administrator', 'text_mute_members'], author, target)
+
+                if not target.guild == guild:
+                    return ChannelResponse(content='*Passed Channel must belong to the current Guild*')
+
+                if not target_perms:
+                    return ChannelResponse(content='**INVALID Permissions**: {} does not have the `administrator` or `text_mute_members` permission in {}.'.format(author.display_name, target.mention))
+
+                if reason:
+                    if not len(reason) >= len(target.mention + ' '):
+                        reason = None
+                    else:
+                        reason = reason[len(target.mention + ' '):]
+
+        if (not target) and (not guild_perms):
+            target = channel
+
+        log_channel = self.get_log_channel(guild)
+
+        if target:
+            if not target.permissions_for(user).send_messages == False:
+                return ChannelResponse(content='*{} already has messaging permissions in {}*'.format(user, target.mention))
+
+            mute_message = await channel.send(content=self.filter_mentions('Un-Muting {} in {}...'.format(user.display_name, target.mention)), delete_after=20)
+
+            try:
+                await target.set_permissions(user, reason='REMOVE TEXT-MUTE: {}'.format(reason), send_messages=None)
+            except:
+                await mute_message.edit(content=self.filter_mentions('Error Un-Muting {} in {}.'.format(user.display_name, target.mention)))
+                return
+
+            await mute_message.edit(content=self.filter_mentions('Un-Muted {} in {}.'.format(user.display_name, target.mention)))
+
+            if log_channel:
+                log_embed = discord.Embed(color=discord.Colour.red()).set_author(name='UN-MUTE: {} ({}#{})'.format(user.display_name, user.name, user.discriminator), icon_url=user.avatar_url)
+                if reason:
+                    log_embed.add_field(name='Reason', value=reason, inline=False)
+                log_embed.add_field(name='Channel', value=target.mention, inline=False).add_field(name='Responsible Moderator', value=author.display_name, inline=False)
+                await log_channel.send(embed=log_embed)
+
+            return
+        else:
+            mute_message = await channel.send(content=self.filter_mentions('Un-Muting {}...'.format(user.display_name)), delete_after=20)
+
+            error_channels = []
+
+            if channel.permissions_for(user).send_messages == False:
+                try:
+                    await channel.set_permissions(user, reason='REMOVE TEXT-MUTE: {}'.format(reason), send_messages=None)
+                except:
+                    error_channels += [channel]
+
+            for target in guild.text_channels:
+                if target.permissions_for(user).send_messages == False and not channel == target:
+                    try:
+                        await target.set_permissions(user, reason='REMOVE TEXT-MUTE: {}'.format(reason), send_messages=None)
+                    except:
+                        error_channels += [target]
+
+            if error_channels:
+                await mute_message.edit(content=self.filter_mentions('Unable to un-mute {} in {} of {} channels'.format(user, len(error_channels), len(guild.text_channels))))
+            else:
+                await mute_message.edit(content=self.filter_mentions('Un-Muted {}...'.format(user.display_name)))
+
+            if log_channel:
+                log_embed = discord.Embed(color=discord.Colour.red()).set_author(name='UN-MUTE: {} ({}#{})'.format(user.display_name, user.name, user.discriminator), icon_url=user.avatar_url)
+                if reason:
+                    log_embed.add_field(name='Reason', value=reason, inline=False)
+                log_embed.add_field(name='Responsible Moderator', value=author.display_name, inline=False)
+                await log_channel.send(embed=log_embed)
+
+            return
