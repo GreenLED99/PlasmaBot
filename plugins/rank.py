8ebc9347ef30bc772829a6912c2af6e8a5eed813
SAL 9000 <18272@jcpstudents.org>
2017-08-18 18:48:46 -0500

    Changes
    added todo, waiting on new PlasmaBot version to be fixed

--- plugins/rank.py
+++ plugins/rank.py
@@ -7,27 +7,36 @@ from plasmaBot.utils import databaseTable
 import discord
 import random
 
-RANK_LOG = databaseTable(['USER_ID','xp', 'rank'],
-                                ['INT PRIMARY KEY NOT NULL', 'INT PRIMARY KEY NOT NULL', 'TEXT NOT NULL'])
+RANK_LOG = databaseTable(['USER_ID','xp', 'level', 'rank', 'cooldown'], ['INT PRIMARY KEY NOT NULL', 'INT', 'INT','STRING','INT'])
 
 class Rank(Plugin):
-	"""A Plugin used to track activity on the server"""
-	
-	NAME = 'rank'
-	
-	def on_plugin_load(self):
+    """A Plugin used to track activity on the server"""
+
+    NAME = 'rank'
+    help_exlcude = True
+
+    def on_plugin_load(self):
         """Event fired on plugin load.  Initializes Plugin elements."""
         self.db = sq.Connect('plasmaBot/data/rank')
 
         if not self.db.table('rank').tableExists():
             self.db.table('rank').init(RANK_LOG)
-	
-	@event
-	async def on_message(self, message):
-		"""PLUGIN EVENT: Client.on_message()"""
-		prefix = self.config['presence']['prefix']
-		if not message.content == '':
-			if author.id != db.table
-			xp_add = db.table("rank").where("USER_ID").equals(author.id).execute()
-			xp_add + random.randint(1,101)
-			db.table("rank").update("xp").setTo("xp_add").where("USER_ID").equals(author.id).execute()
+
+    @event
+    async def on_message(self, message):
+        """PLUGIN EVENT: Client.on_message()"""
+        prefix = self.config['presence']['prefix']
+        """
+        TO DO:
+                ~Add users to database
+                    -Check if user is in db before XP added
+                ~learn timer function for cooldown
+                    -skip XP add when timer is not at 0 and reset when XP added
+                ~add customizable values for XP that can be set in config 
+                    -(LOW PRIORITY)
+        """
+                
+        if not message.content == '':
+            xp_add = db.table("rank").where("USER_ID").equals(author.id).execute()
+            xp_add + random.randint(1,101) #values customizable to whatever is wanted
+            db.table("rank").update("xp").setTo("xp_add").where("USER_ID").equals(author.id).execute() #update the database with new XP
