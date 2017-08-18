5a2c5e54e73b8763e0b8a750c4da3ed194c033de
SAL 9000 <18272@jcpstudents.org>
2017-08-18 13:49:25 -0500

    Added incomplete random xp_add

--- plugins/rank.py
+++ plugins/rank.py
@@ -5,9 +5,10 @@ from SQLiteHelper import SQLiteHelper as sq
 from plasmaBot.utils import databaseTable
 
 import discord
+import random
 
 RANK_LOG = databaseTable(['USER_ID','xp', 'rank'],
-                                ['INT PRIMARY KEY NOT NULL', 'TEXT NOT NULL', 'TEXT NOT NULL'])
+                                ['INT PRIMARY KEY NOT NULL', 'INT PRIMARY KEY NOT NULL', 'TEXT NOT NULL'])
 
 class Rank(Plugin):
 	"""A Plugin used to track activity on the server"""
@@ -20,10 +21,13 @@ class Rank(Plugin):
 
         if not self.db.table('rank').tableExists():
             self.db.table('rank').init(RANK_LOG)
-
+	
 	@event
-    async def on_message(self, message):
-        """PLUGIN EVENT: Client.on_message()"""
-        prefix = self.config['presence']['prefix']
-        if not message.content == '':
-            db.table("rank").update("rank").setTo().where("USER_ID").equals(user.id).execute()
+	async def on_message(self, message):
+		"""PLUGIN EVENT: Client.on_message()"""
+		prefix = self.config['presence']['prefix']
+		if not message.content == '':
+			if author.id != db.table
+			xp_add = db.table("rank").where("USER_ID").equals(author.id).execute()
+			xp_add + random.randint(1,101)
+			db.table("rank").update("xp").setTo("xp_add").where("USER_ID").equals(author.id).execute()
